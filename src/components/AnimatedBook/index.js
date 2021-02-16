2aa50db36fa47ab88b719f590c0700b2b63d0127
xjl <1944559669@qq.com>
2021-02-16 22:12:53 +0800

    flx:user

---
+++ src/components/AnimatedBook/index.js
@@ -0,0 +1,31 @@
+// import React from "react";
+// class AnimatedBook extends React.Component {
+//   render() {
+//     return (
+//       {cover,content}=this.props;
+//       <div className="book-container">
+//         <div className="book">
+//           {/* 封面 */}
+//           <ul className="hardcover_front">
+//             <li>{cover}</li>
+//             <li></li>
+//           </ul>
+//           {/* 书页 */}
+//           <ul className="page">
+//             <li></li>
+//             <li>{content}</li>
+//             <li></li>
+//             <li></li>
+//           </ul>
+//           {/* 背面 */}
+//           <ul className="hardcover_back">
+//             <li></li>
+//             <li></li>
+//           </ul>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default AnimatedBook;
---
+++ src/components/Icon/index.js
@@ -0,0 +1,7 @@
+import {Icon} from 'antd'
+
+const MyIcon=Icon.createFromIconfontCN({
+  scriptUrl:'//at.alicdn.com/t/font_2172243_73ax6kn90rc.js'
+})
+
+export default MyIcon
